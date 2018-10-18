aee503efdbbcd2dceff6ac10b2ddcf3d32cbe668
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2018-10-18 21:07:23 +0200

    added Zuul proxy gateway

---
+++ djk-gateway/src/main/java/pl/krakow/uek/djkgateway/DjkGatewayApplication.java
@@ -0,0 +1,16 @@
+package pl.krakow.uek.djkgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class DjkGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjkGatewayApplication.class, args);
+	}
+}
---
+++ djk-gateway/src/test/java/pl/krakow/uek/djkgateway/DjkGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.krakow.uek.djkgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DjkGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
